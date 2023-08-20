1bb7c959e2a781af408eddf023b6b7cab48e40e8
okewunmi <okewunmiafeezolaide@gmail.com>
2023-08-20 03:48:32 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="main">
+      <df-messenger
+        intent="WELCOME"
+        chat-title="GlobalNet Nexus"
+        agent-id="be6fb2e4-16e2-43de-a079-35fcbb417d9c"
+        language-code="en"
+      ></df-messenger>
+    </div>
+  );
+}
+
+export default App;

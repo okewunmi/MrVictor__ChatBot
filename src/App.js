d4549589035847789f2d15277962befcb5c5e235
okewunmi <okewunmiafeezolaide@gmail.com>
2023-08-22 13:10:52 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
+import React from "react";
 import "./App.css";
+
+import imagebg from "./bck-.jpg";
 
 function App() {
   return (
-    <div className="main">
-      <df-messenger
-        intent="WELCOME"
-        chat-title="GlobalNet Nexus"
-        agent-id="be6fb2e4-16e2-43de-a079-35fcbb417d9c"
-        language-code="en"
-      ></df-messenger>
-    </div>
+    <main
+      style={{
+        backgroundImage: `url(${imagebg})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+    >
+      <header>
+        <a
+          className="link"
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://globalnetexperts.com.ng"
+        >
+          <p>Back to home</p>
+        </a>
+      </header>
+      {/* <img src={imagebg} alt="background" className="bck__image" /> */}
+      <div className="main">
+        <df-messenger
+          intent="WELCOME"
+          chat-title="GlobalNet Nexus"
+          agent-id="be6fb2e4-16e2-43de-a079-35fcbb417d9c"
+          language-code="en"
+        ></df-messenger>
+      </div>
+    </main>
   );
 }
 
